2112f7ab1b61bc63ff1c9c7cc1b2dc4d79695952
Prafull Mogdul <136841994+prafullmogdul1009@users.noreply.github.com>
2026-09-14 22:42:30 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,11 @@
+variable=12
+print(type(variable))
+variable=12.43
+
+print(type(variable))
+variable=True
+print(type(variable))
+variable="242"
+print(type(variable))
+variable=[1,3,5,5]
+print(type(variable))

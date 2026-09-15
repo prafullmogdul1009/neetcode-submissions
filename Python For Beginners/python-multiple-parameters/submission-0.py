b06446e77c9591ceee10c76e44feba1343316fbc
Prafull Mogdul <136841994+prafullmogdul1009@users.noreply.github.com>
2026-09-15 14:18:57 +0530

    Add: python-multiple-parameters - submission-0

---
+++ Python For Beginners/python-multiple-parameters/submission-0.py
@@ -0,0 +1,11 @@
+def two_sum(*args):
+    print(sum(args))
+
+def three_sum(*args):
+    print(sum(args))
+
+# do not modify below this line
+print(17)
+print(16)
+two_sum(10, 9)
+three_sum(5, 14, 6)
